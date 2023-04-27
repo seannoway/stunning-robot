c62fef805fac12d9e622cfbee379bee841df6168
Sean Noway <sean.m.holian@gmail.com>
2023-04-27 15:29:06 -0400

    Added function for character amount and added blank password var

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -1,10 +1,32 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
 //sets variables for the password generator to access
 var upper = 'ABCDEFGHIJKLMONOPQRSTUVWXYZ';
 var lower = 'abcdefghijklmnopqrstuvwxyz';
 var number = '1234567890';
 var special = `!"#$%&'()*+,-./:;<=>?@[\]^_{|}~`;
+var password = "";
+
+//defines a function that prompts the uder to select a character amount in the window between 8 and 128
+function charAmount() {
+  result = parseInt(window.prompt('Please enter a character amount'));
+// checks to see if the number selected is between 8 and 128
+  if (!Number.isInteger(result) || result <= 7 || result >= 129) {
+    window.alert('Please enter a number beween 8 and 128');
+    return charAmount();
+  } else {
+    return result;
+  }
+  }
+
+  function inputConfirm(passwordText) {
+    if (window.confirm(passwordText)) {
+      return true;
+    }
+  };
+
+
 
 
 
